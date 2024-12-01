405b279add391c494080807edd48a4848352c7b1
milagro <zv23001@ues.edu.sv>
2024-11-30 22:45:06 -0600

    Primera prueba

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/cineclient/CineClient.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.cineclient;
+
+/**
+ *
+ * @author milag
+ */
+public class CineClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // PRIMERA PRUEBA PARA COMMIT
+    }
+    
+}
